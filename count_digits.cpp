d440924f6a8c65602f7352e1a1a9ad33e3bd7dee
vicky750hnn <95040397+vicky750hnn@users.noreply.github.com>
2022-08-07 11:50:38 +0530

    changed a while loop to for loop

--- count_digits.cpp
+++ count_digits.cpp
@@ -4,8 +4,8 @@ int main(){
     int n,sum;
     cout << "Enter the number to check : ";
     cin >> n;
-    while(n/10){
-        sum++;
+    for(int x=0;x<n;x++){
+        cout << "Hello\n";
     }
     cout << "The number of digits is" << sum << endl;
     return 0;
