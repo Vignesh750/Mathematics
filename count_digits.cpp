b0ec7ed116372159f8ee06869fcc67b34113be26
vicky750hnn <95040397+vicky750hnn@users.noreply.github.com>
2022-08-08 11:02:22 +0530

    Program to count no. of digits in a number
    Three methods are used to find the solution

--- count_digits.cpp
+++ count_digits.cpp
@@ -1,12 +1,34 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
-int main(){
-    int n,sum;
-    cout << "Enter the number to check : ";
-    cin >> n;
-    for(int x=0;x<n;x++){
-        cout << "Hello\n";
+
+
+// To find the number of digits in a given number
+// Three methods can be used to find the number of digits in a number
+// Iterative method
+// Recursive method 
+// Logarithmic method
+//The  logarithmic method is a one liner
+int iterative(int n){
+    int count=0;
+    while(n!=0){
+        count++;
+        n= n/10;
     }
-    cout << "The number of digits is" << sum << endl;
+    return count;
+}
+int recursive(int n){
+    if(n==0){
+        return 0;
+    }
+    return 1+recursive(n/10);
+}
+int logarithmic(int n){
+    return floor(log10(n)+1);
+}
+int main(){
+    cout << "iterative method"<< iterative(123) << endl;
+    cout << "recursive method"<< recursive(123) << endl;
+    cout << "logarithmic method"<< logarithmic(123) << endl;
     return 0;
 }
